9607ba316f312d25282e2b71170b486ac3f0d392
nekuro201 <guilherme.kodi.abe@gmail.com>
2024-10-24 11:34:59 +0900

    header of container tasks, finished header main

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,54 @@
 import logo from './assets/todo-logo.svg'
+import plusIcon from './assets/plus.svg'
 
 import styles from './app.module.css'
+import { FormEvent, useState } from 'react'
 
 export function App() {
+
+  const [task, setTask] = useState('')
+
+  function handleSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+
+    setTask('')
+  }
+
   return (
     <div className={styles.main}>
       <div className={styles.header}>
         <img src={logo} className="logo" alt="Vite logo" />
 
-        <form>
-          <input type='text' name='task' />
-          <button>Criar</button>
+        <form onSubmit={handleSubmit}>
+          <input 
+            type='text' 
+            name='task' 
+            value={task} 
+            onChange={(e) => setTask(e.target.value)}
+            placeholder='Adicione uma nova tarefa'
+          />
+          <button type='submit'>
+            Criar
+            <img src={plusIcon} />
+          </button>
         </form>
+      </div>
+
+      <div className={styles.tasksContainer}>
+        <div className={styles.tasksHeader}>
+          <div>
+            <span>Tarefas criadas</span>
+            <span className={styles.resultBox}>0</span>
+          </div>
+          <div>
+            <span>Tarefas criadas</span>
+            <span className={styles.resultBox}>0</span>
+          </div>
+        </div>
+
+        <div className='tasks'>
+          
+        </div>
       </div>
     </div>
   )
