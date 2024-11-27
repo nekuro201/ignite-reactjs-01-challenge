bafa6aad6bcb66888f332cce8ea41ab3babe371b
nekuro201 <guilherme.kodi.abe@gmail.com>
2024-11-28 03:28:48 +0900

    finished all functionalities

--- src/App.tsx
+++ src/App.tsx
@@ -8,14 +8,61 @@ import trashIcon from './assets/trash.svg'
 
 import styles from './app.module.css'
 
+interface Task {
+  createdAt: string
+  text: string
+  isFinished?: boolean
+}
+
 export function App() {
 
-  const [task, setTask] = useState('')
+  const [taskInput, setTaskInput] = useState("")
+  const [tasks, setTasks] = useState<Task[]>([])
 
   function handleSubmit(e: FormEvent<HTMLFormElement>) {
     e.preventDefault()
 
-    setTask('')
+    const date = new Date().toISOString()
+
+    if (!taskInput) return
+    
+    setTasks(prevTasks => {
+      const newTask: Task = {
+        createdAt: date,
+        text: taskInput,
+        isFinished: false
+      }
+
+      return [newTask, ...prevTasks]
+    })
+
+    setTaskInput('')
+  }
+
+  function handleCheckFinished(createdAt: string) {
+    setTasks(prevTasks => {
+      return prevTasks.map(task => {
+        if (task.createdAt === createdAt)
+          task.isFinished = true
+        return task
+      })
+    })
+  }
+
+  function handleUncheckFinished(createdAt: string) {
+    setTasks(prevTasks => {
+      return prevTasks.map(task => {
+        if (task.createdAt === createdAt)
+          task.isFinished = false
+        return task
+      })
+    })
+  }
+
+  function handleDeleteTask(createdAt: string) {
+    setTasks(prevTasks => {
+      return prevTasks.filter(task => task.createdAt !== createdAt)
+    })
   }
 
   return (
@@ -28,10 +75,10 @@ export function App() {
         <form onSubmit={handleSubmit}>
           <input 
             type='text' 
-            name='task' 
-            value={task} 
-            onChange={(e) => setTask(e.target.value)}
+            name='task-input' 
             placeholder='Adicione uma nova tarefa'
+            value={taskInput}
+            onChange={e => setTaskInput(e.target.value)}
           />
           <button type='submit'>
             Criar
@@ -42,50 +89,40 @@ export function App() {
         <div className={styles.tasksHeader}>
           <div>
             <span>Tarefas criadas</span>
-            <span className={styles.resultBox}>5</span>
+            <span className={styles.resultBox}>{tasks.length}</span>
           </div>
           <div>
             <span>Tarefas criadas</span>
-            <span className={styles.resultBox}>2 de 5</span>
+            <span className={styles.resultBox}>{tasks.filter(task => task.isFinished == true).length} de {tasks.length}</span>
           </div>
         </div>
 
         <div className={styles.tasks}>
-          <div className={`${styles.task}`}>
-            <div>
-              <button>
-                <img src={uncheckIcon} style={{width: 24, height: 24}}/>
-              </button>
+          {tasks.map(task => (
+            <div key={task.createdAt} className={`${styles.task} ${task.isFinished && styles.finished}`}>
+              <div>
+                {task.isFinished ? (
+                  <button onClick={() => handleUncheckFinished(task.createdAt)}>
+                      <img src={checkIcon} style={{width: 24, height: 24}}/>
+                  </button>
+                ) : (
+                  <button onClick={() => handleCheckFinished(task.createdAt)}>
+                    <img src={uncheckIcon} style={{width: 24, height: 24}}/>
+                  </button>
+                )}
+              </div>
+
+              <p>
+                {task.text}
+              </p>
+
+              <div>
+                <button onClick={() => handleDeleteTask(task.createdAt)}>
+                  <img src={trashIcon} style={{width: 24, height: 24}}/>
+                </button>
+              </div>
             </div>
-
-            <p>
-              aaa aaaaaaaaaaaa aaa aaaaaaaaa aaaaaaaaaaaa aaa aaaaaaaaa aaaaaaaaaaaa aaa aaaaaaaaa aaaaaaaaaaaa aaa aaaaaaaaa aaaaaaaaaaaa aaa aaaaaaaaa aaaaaaaaaaaa aaa aaaaaa
-            </p>
-
-            <div>
-              <button>
-                <img src={trashIcon} style={{width: 24, height: 24}}/>
-              </button>
-            </div>
-          </div>
-
-          <div className={`${styles.task} ${styles.finished}`}>
-            <div>
-              <button>
-                <img src={checkIcon} style={{width: 24, height: 24}}/>
-              </button>
-            </div>
-
-            <p>
-              aaa aaaaaaaaaaaa aaa aaaaaaaaa aaaaaaaaaaaa aaa aaaaaaaaa aaaaaaaaaaaa aaa aaaaaaaaa aaaaaaaaaaaa aaa aaaaaaaaa aaaaaaaaaaaa aaa aaaaaaaaa aaaaaaaaaaaa aaa aaaaaa
-            </p>
-
-            <div>
-              <button>
-                <img src={trashIcon} style={{width: 24, height: 24}}/>
-              </button>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </div>
